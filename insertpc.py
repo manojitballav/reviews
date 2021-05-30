dda9178cc67c02c15810064a191ea19d8ae3fd9a
manojitballav <manojitocto@gmail.com>
2021-05-30 18:41:25 +0530

    Fixed update to db for new models

--- insertpc.py
+++ insertpc.py
@@ -1,6 +1,9 @@
 import re,time
 from pymongo import MongoClient
 
+# calling custom methods
+from amazon import *
+
 client = MongoClient('127.0.0.1',27017)
 db = client['amazon']
 col = db['record']
@@ -14,12 +17,14 @@ def dbinsert(data):
     color = data[4]
     # col.insert_one({'name':name,'asin':pc,'ram':ram,'rom':rom,'color':color})
     if (col.count_documents( { 'asin': pc },limit=1)== 0):
-        col.update_one({'asin':pc},{"$set":{'name':name,'asin':pc,'ram':ram,'rom':rom,'color':color}})
-        return (devdata)
+        col.update_one({'asin':pc},{"$set":{'name':name,'asin':pc,'ram':ram,'rom':rom,'color':color}},upsert=True)
+        # calling function to initiate data scraping
+        # number(devdata)
+        return (number(devdata))
     else:
         devdata = (pc +str(' Exists !'))
         return (devdata)
     # print(name,pc,ram,rom,color)
 
-    8367043307
+   
     
